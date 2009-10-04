9ae48e526430d4120ac1b052baefb7b81cd1b8d2
Joel Burton <private@example.org>
2009-10-04 15:12:35 +0000

    Moved to subclassing nested namespace, since this is a nested namespace, and was having to pull in the NS2 question before.

--- zopeskel/kss_plugin.py
+++ zopeskel/kss_plugin.py
@@ -3,26 +3,21 @@ import copy
 
 from zopeskel.base import get_var
 from zopeskel.base import var
-from zopeskel.basic_namespace import BasicNamespace
-from zopeskel.nested_namespace import VAR_NS2
+from zopeskel.nested_namespace import NestedNamespace
 
 
-class KssPlugin(BasicNamespace):
+class KssPlugin(NestedNamespace):
     _template_dir = 'templates/kss_plugin'
     summary = "A project for a KSS plugin"
+    ndots = 2
     help = """
 This creates a project for a KSS plugins ('Kinetic Style Sheets', a 
 Plone 3 framwork for JavaScript/AJAX).
-
-This template expects a name in the form 'mycompany.kss.example'
-(2 dot, a 'nested namespace'); you cannot have a flat package name 
-(no dots, 'myproduct') or a single namespace (1 dot, 'mycompany.product').
 """
 
     required_templates = []
     use_cheetah = True
 
-    vars = copy.deepcopy(BasicNamespace.vars)
+    vars = copy.deepcopy(NestedNamespace.vars)
     get_var(vars, 'namespace_package').default = 'kss'
-    vars.insert(2, VAR_NS2)
     get_var(vars, 'keywords').default = 'kss plugin'
