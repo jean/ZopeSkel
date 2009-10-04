0d933c64edd98e49e29a1b9695e76422d16ad787
Chris Rossi <private@example.org>
2009-10-04 14:56:03 +0000

    Factor out common ui components to separate module.

--- zopeskel/zopeskel_script.py
+++ zopeskel/zopeskel_script.py
@@ -2,25 +2,7 @@ import sys
 import pkg_resources
 from paste.script.command import get_commands
 
-
-# These are the "common" templates; they will be listed in a separate
-# list for new users. Please be conservative about adding new
-# templates to this list--we don't want it to be overwhelming.
-# Users also see the "non-common" templates, just in a different
-# listing; this is where things like PAS plugins, special hosting,
-# Silva, etc., should remain.
-
-COMMON = [
-  'archetype',
-  'plone',
-  'plone2.5_buildout',
-  'plone2.5_theme',
-  'plone2_theme',
-  'plone3_buildout',
-  'plone3_portlet',
-  'plone3_theme',
-  'plone_app',
-]
+from zopeskel.ui import list_sorted_templates
 
 USAGE = """
 Usage:
@@ -39,7 +21,7 @@ option "--help".
 
 DESCRIPTION = """
 This script allows you to create basic skeletons for plone and zope
-products and buildouts based on best-practice templates. 
+products and buildouts based on best-practice templates.
 
 It is a wrapper around PasteScript ("paster"), providing an easier
 syntax for invoking and better help.
@@ -125,46 +107,12 @@ Notes:
 QUESTIONS
 
 If you have further questions about the usage of bin/zopeskel, please feel
-free to post your questions to the zopeskel mailing list or jump onto the 
+free to post your questions to the zopeskel mailing list or jump onto the
 plone IRC channel (#plone) at irc.freenode.net.
 
 
 To see the templates supported, run this script without any options.
 """
-
-def list_sorted_templates():
-    """Output a printable list of all templates, sorted into two parts.
-        
-    Templates will be sorted into 'common' and 'advanced' groups
-    and listed separately.
-    """
-    common_list = ""
-    advanced_list = ""
-    # grab a list of all paster create template entry points
-    t_e_ps = pkg_resources.iter_entry_points('paste.paster_create_template')
-    templates = []
-    for entry in t_e_ps:
-        try:
-            templates.append(entry.load()(entry.name))
-        except Exception, e:
-            # We will not be stopped!
-            print 'Warning: could not load entry point %s (%s: %s)' % (
-                entry.name, e.__class__.__name__, e)
-    max_name = max([len(t.name) for t in templates])
-    templates.sort(lambda a, b: cmp(a.name, b.name))
-    
-    for template in templates:
-        name = template.name
-        display = "|  %s:%s %s\n" % (
-            name,
-            ' '*(max_name-len(template.name)),
-            template.summary)
-        if name in COMMON:
-            common_list += display
-        else:
-            advanced_list += display
-    
-    return common_list, advanced_list
 
 def usage():
     common, uncommon = list_sorted_templates()
@@ -175,7 +123,7 @@ def help():
 
 def run():
     """ """
-    
+
     if "--help" in sys.argv:
         help()
         return
