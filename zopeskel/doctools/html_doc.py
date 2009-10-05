b323a633515b2028fc078e1865a5ef4751fe0a0d
Joel Burton <private@example.org>
2009-10-05 15:49:08 +0000

    Added list of local commands to auto-generated HTML docs.

--- zopeskel/doctools/html_doc.py
+++ zopeskel/doctools/html_doc.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
 
+"""HTML documentation generator.
+
+A quick-n-dirty generator of HTML docs on templates, including their
+fields and subtemplates.
+"""
+
+# This should be changed into generating data structures, and having
+# Cheetah templates to render them
+
 from zopeskel.ui import list_sorted_templates
+import pkg_resources
+
+def _get_local_commands():
+    """Return dict of main_template -> [ local template1, ... localN ]"""
+
+    out = {}
+    for entry in pkg_resources.iter_entry_points('zopeskel.zopeskel_sub_template'):
+        localc = entry.load()
+        for parent in localc.parent_templates:
+            out.setdefault(parent, []).append((entry.name, localc.summary))
+    return out
 
 def make_html():
 
     common, advanced = list_sorted_templates()
+    subtemplates = _get_local_commands()
+
     for title, list_ in (
             ('Common Recipes', common), 
             ('Advanced Recipes', advanced)):
@@ -16,6 +38,7 @@ def make_html():
             help = getattr(tempc, 'help', '')
             for para in help.split("\n\n"):
                 print '<p>%s</p>' % para
+            print "<h4>Fields:</h4>"
             print "<ul>"
             for var in tempc.vars:
                 if hasattr(var, 'pretty_description'):
@@ -23,6 +46,13 @@ def make_html():
                 else:
                     print "<li>%s</li>" % var.name
             print "</ul>"
+            subs = subtemplates.get(temp.name)
+            if subs:
+                print "<h4>Sub Templates:</h4>"
+                print "<ul>"
+                for sub in subs:
+                    print "<li>%s (%s)</li>" % (sub[0], sub[1])
+                print "</ul>"
             
 if __name__=="__main__":
     make_html()
