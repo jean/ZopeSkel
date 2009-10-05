7ff0695c650af27dadbf0de8da31e246610612cc
Joel Burton <private@example.org>
2009-10-05 17:53:44 +0000

    Added verbosely detailed listing of templates.

--- zopeskel/zopeskel_script.py
+++ zopeskel/zopeskel_script.py
@@ -1,5 +1,7 @@
 import sys
 import pkg_resources
+from textwrap import TextWrapper
+from zopeskel.base import wrap_help_paras
 from paste.script.command import get_commands
 
 from zopeskel.ui import list_sorted_templates
@@ -10,6 +12,7 @@ Usage:
     zopeskel <template> <output-name> [var1=value] ... [varN=value]
 
     zopeskel --help                Full help
+    zopeskel --list                List template verbosely, with details
     zopeskel --make-config-file    Output .zopeskel prefs file
 
 Common templates:
@@ -30,52 +33,66 @@ It is a wrapper around PasteScript ("paster"), providing an easier
 syntax for invoking and better help.
 
 
-INVOKING THIS SCRIPT:
+Invoking this script
+--------------------
 
-Basic usage:
+Basic usage::
 
-    bin/zopeskel <template>
+    zopeskel <template>
 
-For example:
+(To get a list of the templates, run the script without any arguments;
+for a verbose list with full descriptions, run the ``zopeskel --list``)
 
-    bin/zopeskel archetypes
+For example::
+
+    zopeskel archetypes
 
 To create an Archetypes-based product for Plone. This will prompt you
 for the name of your product, and for other information about it.
 
 If you to specify your output name (resulting product, egg, or buildout,
-depending on the template being used), you can also do so:
+depending on the template being used), you can also do so::
 
     zopeskel <template> <output-name>
 
-For example:
+For example::
 
     zopeskel archetypes Products.Example
 
 In addition, you can pass variables to this that would be requested
 by that template, and these will then be used. This is an advanced
-feature mostly useful for scripted use of this:
+feature mostly useful for scripted use of this::
 
     zopeskel archetypes Products.Example author_email=joel@joelburton.com
 
 (You can specify as many of these as you want, in name=value pairs.
 To get the list of variables that a template expects, you can ask for
-this with "paster create -t <template-name> --list-variables").
+this with ``paster create -t <template-name> --list-variables``).
 
 
-ANSWERING QUESTIONS:
+Interactive Help
+----------------
 
 While being prompted on each question, you can enter with a single
-question mark to receive interactive help for that question.
+question mark to receive interactive help for that question. 
+
+For example::
+
+  Description (One-line description of the project) ['']: ?
+
+  |  This should be a single-line description of your project. It will
+  |  be used in the egg's setup.py, and, for Zope/Plone projects, may be
+  |  used in the GenericSetup profile description.
 
 
-PROVIDING DEFAULTS:
+Providing defaults
+------------------
 
 It is also possible to set up default values to be used for any template by
-creating a file called '.zopeskel' in your home directory. This file
+creating a file called ``.zopeskel`` in your home directory. This file
 should be in INI format.
 
-For example, our $HOME/.zopeskel could contain:
+For example, our ``$HOME/.zopeskel`` could contain::
 
     [DEFAULT]
     author_email = joel@joelburton.com
@@ -89,7 +106,7 @@ For example, our $HOME/.zopeskel could contain:
 
 You can generate a starter .zopeskel file by running this script with
 the --make-config-file option. This output can be redirected into
-your .zopeskel file:
+your ``.zopeskel`` file::
 
     bin/zopeskel --make-config-file > /path/to/home/.zopeskel
 
@@ -104,7 +121,7 @@ Notes:
    for all templates.
 
 3) We can make a setting in DEFAULT and then override it for a
-   particular template. In this example, we generally prefer the GPL,
+   particular template. In this example, we might generally prefer the GPL,
    but issue our themes under the BSD license.
 
 4) You can refer to variables from the same section or from the
@@ -113,7 +130,8 @@ Notes:
    for the theming template by referring to the master list.
 
 
-QUESTIONS
+Questions
+---------
 
 If you have further questions about the usage of bin/zopeskel, please feel
 free to post your questions to the zopeskel mailing list or jump onto the
@@ -121,6 +139,7 @@ plone IRC channel (#plone) at irc.freenode.net.
 
 
 To see the templates supported, run this script without any options.
+For a verbose listing with help, use ``zopeskel --list``.
 """
 
 DOT_HELP = {
@@ -171,6 +190,28 @@ def usage():
 
 def show_help():
     print DESCRIPTION
+
+def list_verbose():
+    """List templates verbosely, with full help."""
+
+    textwrapper = TextWrapper(
+            initial_indent="   ", subsequent_indent="   ")
+    common, advanced = list_sorted_templates()
+
+    for title, list_ in (
+        ('Common Templates', common),
+        ('Advanced Templates', advanced)):
+        print "\n"+ title
+        print "-" * len(title)
+        for temp in list_:
+            tempc = temp.load()
+            print "\n%s: %s\n" % (temp.name, tempc.summary)
+
+            help = getattr(tempc, 'help', None)
+            if help:
+                wrap_help_paras(textwrapper, help.strip())
+    print
+
 
 def list_printable_templates():
     """
@@ -243,12 +284,16 @@ def process_args():
 def run():
     """ """
 
+    if "--help" in sys.argv:
+        show_help()
+        return
+
     if "--make-config-file" in sys.argv:
         generate_dotzopeskel()
         return
 
-    if "--help" in sys.argv:
-        show_help()
+    if "--list" in sys.argv:
+        list_verbose()
         return
 
     if len(sys.argv) == 1:
